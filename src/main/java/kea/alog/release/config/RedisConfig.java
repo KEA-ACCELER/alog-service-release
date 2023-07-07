668f67440ca6cf065a166c1c71d19b464b74c1f4
Kang Hyun Mo <kangstar96@gmail.com>
2023-07-07 02:21:08 +0000

    Add RedisConfig

---
+++ src/main/java/kea/alog/release/config/RedisConfig.java
@@ -0,0 +1,19 @@
+package kea.alog.release.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+public class RedisConfig {
+    
+    private int port = 6379;
+    
+    private String host = "localhost";
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(){
+        return new LettuceConnectionFactory(host, port);
+    }
+}
